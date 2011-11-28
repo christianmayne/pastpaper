9a6ba539ecab5ce724ac026815a4141aed80702f
Bishnu Subedi <bisusubedi@gmail.com>
2011-11-28 17:38:11 +0545

    inflection and admin seed

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -3,7 +3,7 @@
 # Add new inflection rules using the following format
 # (all these examples are active by default):
  ActiveSupport::Inflector.inflections do |inflect|
-  inflect.singular("status", "status")
+ # inflect.singular("statuses", "status")
 #   inflect.plural /^(ox)$/i, '\1en'
 #   inflect.singular /^(ox)en/i, '\1'
 #   inflect.irregular 'person', 'people'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-User.create(:first_name=>"admin" ,:last_name=>"admin",:username=>"admin",:email=>"admin@pastonpaper.com",:password=>"123456",:password_confirmation=>"123456")
+User.create(:first_name=>"admin" ,:last_name=>"admin",:username=>"adminuser",:email=>"admin@pastonpaper.com",:password=>"123456",:password_confirmation=>"123456")
