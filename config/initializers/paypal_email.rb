78be7559963fd9dcd17ffcabf5cb7761f12d5d7e
Bishnu Subedi <bisusubedi@gmail.com>
2011-12-15 12:27:35 +0545

    setting paypal in test mode

--- config/initializers/paypal_email.rb
+++ config/initializers/paypal_email.rb
@@ -1,5 +1,6 @@
 if Rails.env.production?
   PAYPAL_ACCOUNT = 'bibek_1316595699_biz@gmail.com'
+  ActiveMerchant::Billing::Base.mode = :test # for live transaction this line should be comented out or removed
 else
   PAYPAL_ACCOUNT = 'bibek_1316595699_biz@gmail.com'
   ActiveMerchant::Billing::Base.mode = :test
