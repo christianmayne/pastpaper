2b842a4e0488175af57c2eec3e249b9a2af47ee6
Bishnu Subedi <bisusubedi@gmail.com>
2011-12-27 17:24:39 +0545

    added scoped validation if event type is added for person it will now allow to add the person's event

--- app/models/person_event.rb
+++ app/models/person_event.rb
@@ -2,11 +2,11 @@ class PersonEvent < ActiveRecord::Base
 
   belongs_to :person
   belongs_to :event_type
-
   
   scope :born, :conditions => {:event_type_id => 1}
   
-  validates :event_type_id,:presence => true
+  validates :event_type_id,:presence => true,:uniqueness => {:scope => :person_id ,:message=>"is already added"}
+
   
   def location
     [self.street1, self.street2, self.city, self.county, self.country].delete_if{|ad_elem| ad_elem.blank?}.join(', ')
