442bcccc42bf8bbd1c32820a26f9b0a2963e3e8c
Bishnu Subedi <bisusubedi@gmail.com>
2012-01-12 18:04:12 +0545

    query update

--- app/controllers/admin/report_controller.rb
+++ app/controllers/admin/report_controller.rb
@@ -2,10 +2,10 @@ class Admin::ReportController < ApplicationController
  
   def index
      @people = Person.all
-      @male_people = Person.where("sex=?","MALE")
+      @male_people = Person.where("SEX = ?","MALE")
    
-    @female_people = Person.where("sex=?","FEMALE")
-    @unknown_people = Person.where("sex=?","UNKNOWN")
+    @female_people = Person.where("SEX = ?","FEMALE")
+    @unknown_people = Person.where("SEX = ?","UNKNOWN")
     @unique_surnames = Person.count(:last_name,:distinct=>true)
      @surnames = Person.select("last_name,count(*) as last_name_count").group("last_name").where("last_name != ?","")
      
