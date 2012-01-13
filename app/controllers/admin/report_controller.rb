d599da8a0838ba5ce695170516eae6c1ddc6782d
Bishnu Subedi <bisusubedi@gmail.com>
2012-01-13 12:31:36 +0545

    PGSql and mysql compatible query test

--- app/controllers/admin/report_controller.rb
+++ app/controllers/admin/report_controller.rb
@@ -2,12 +2,12 @@ class Admin::ReportController < ApplicationController
  
   def index
      @people = Person.all
-      @male_people = Person.where("SEX = ?","MALE")
+     @male_people = Person.where("UPPER(sex) = ?","MALE".upcase)
    
-    @female_people = Person.where("SEX = ?","FEMALE")
-    @unknown_people = Person.where("SEX = ?","UNKNOWN")
+    @female_people = Person.where("UPPER(sex) = ?","FEMALE".upcase)
+    @unknown_people = Person.where("UPPER(sex) = ?","UNKNOWN".upcase)
     @unique_surnames = Person.count(:last_name,:distinct=>true)
-     @surnames = Person.select("last_name,count(*) as last_name_count").group("last_name").where("last_name != ?","")
+    @surnames = Person.select("last_name,count(*) as last_name_count").group("last_name").where("last_name != ?","").order("last_name")
      
     
   end
