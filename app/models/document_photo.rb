5f69502c52a60ea5bd0d911e3fc3afd912045745
Bishnu Subedi <bisusubedi@gmail.com>
2012-04-24 19:35:22 +0545

    image size fix

--- app/models/document_photo.rb
+++ app/models/document_photo.rb
@@ -1,7 +1,7 @@
 class DocumentPhoto < ActiveRecord::Base
 	belongs_to :document
 	has_attached_file :photo, 
-                    :styles => { :small => '80x80#', :thumb => '150x150>' ,:large=> "600X600>" },
+                    :styles => { :small => '80x80#', :thumb => '150x150#' ,:large=> "600X600>" },
                     :storage => :s3,
                     :s3_credentials => "#{Rails.root}/config/s3.yml",
                     :path => "uploads/:attachment/:id/:style.:extension"
