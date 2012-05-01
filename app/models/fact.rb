f9d0436865cba6dd615826f5acb2fa4b2574bd75
Bishnu Subedi <bisusubedi@gmail.com>
2012-05-01 18:51:57 +0545

    some fixes from tickets

--- app/models/fact.rb
+++ app/models/fact.rb
@@ -3,7 +3,7 @@ belongs_to :person
 belongs_to :event_type
 belongs_to :location
 
-validates :date_modifier ,:presence => true
+#validates :date_modifier ,:presence => true
 
 validates :event_type_id,:presence => true,:uniqueness => {:scope => :person_id ,:message=>"is already added"}
 
