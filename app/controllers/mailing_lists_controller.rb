4873755597faf1ec54fb545ecf0dc4149a23f685
idreams <bisusubedi@gmail.com>
2012-08-20 20:57:44 +0545

    Mailing list handling controller added

---
+++ app/controllers/mailing_lists_controller.rb
@@ -0,0 +1,23 @@
+class MailingListsController < ApplicationController
+  def index
+  end
+
+  def create
+    if request.xhr?
+      #raise params.inspect
+        if !params[:first_name].blank? && !params[:last_name].blank? && !params[:email].blank?
+          # do send mail
+         
+          MailingListMailer.mailinglist_signup_notice_user(params[:first_name],params[:last_name],params[:email]).deliver
+          MailingListMailer.mailinglist_signup_notice_admin(params[:first_name],params[:last_name],params[:email]).deliver
+          
+          @status = true
+          @msg = "Thank you for signing up mailing list"
+        else
+          @status = false
+          @msg = "Please fill all the fields"
+        end
+    end
+  end
+
+end
