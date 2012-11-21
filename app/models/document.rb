863eb54d220f9757ea42a4bf322f52ef1b6a0984
Christian Mayne <christian@christianmayne.net>
2012-11-21 14:36:00 +0000

    new person search stub

--- app/models/document.rb
+++ app/models/document.rb
@@ -157,6 +157,10 @@ class Document < ActiveRecord::Base
 		end
 	end
 
+	def search_people_new(search_params,page,per_page=50)
+  	people = Person.find_all_by_last_name("Jones")
+		#people.each do |person| puts person.document.name
+	end
 
 	def self.search_people(search_params,page,per_page=50)
 		search_params[:date_birth_to] = '0' if search_params[:date_birth_to].blank?
