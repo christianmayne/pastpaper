1532b2bf479c535996d2ef1a762aceb91440287a
Christian Mayne <christian@christian-mayne.net>
2012-12-18 15:06:16 +0000

    Some basic tidy up of document comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,11 @@
 class Comment < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :document
+
+	default_scope order('created_at DESC')
+
+	def date_and_time
+		self.created_at.strftime("%H:%M on %d %B %Y")
+	end
+
 end
