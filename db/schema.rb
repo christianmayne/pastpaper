0d55bb318ce9b24226b8d0a674aba550c50ea34f
Christian Mayne <christian@christian-mayne.net>
2012-12-19 15:38:43 +0000

    Added useragent to document_views and attempt to filter robots

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121219130527) do
+ActiveRecord::Schema.define(:version => 20121219153349) do
 
   create_table "attribute_types", :force => true do |t|
     t.string   "name"
@@ -80,6 +80,7 @@ ActiveRecord::Schema.define(:version => 20121219130527) do
     t.integer  "document_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "useragent"
   end
 
   create_table "documents", :force => true do |t|
