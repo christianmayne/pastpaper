cd38dab6028370f4317fe9906cfc1c4209c13c39
Christian Mayne <christian@christian-mayne.net>
2013-01-20 11:52:29 +0000

    Improved newspaper date filter

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -52,5 +52,14 @@ module ApplicationHelper
 		Location.find :all, :order=>"country ASC", :select => "DISTINCT country"
 	end
 
+	def month_array
+		[["January", "1"],["February", "2"], 
+		["March", "3"],["April",4],
+		["May",5],["June",6],
+		["July",7],["August",8],
+		["September",9],["October",10],
+		["November",11],["December",12]]
+	end
+
 
 end
